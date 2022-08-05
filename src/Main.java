029377d784cfe864f612123e679d36b809278ee2
Nadezcha Frantsuzova <nadia_franc@mail.ru>
2022-08-05 15:42:27 +0300

    Добавление среднего обрезанного с выделенным методом min

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int [] sales = {1, 20, 50, 190, 210, 60, 70};
+        int [] sales = {300, 20, 50, 190, 210, 60, 70};
         SalesManager salesManager = new SalesManager(sales);
 
         System.out.println("Максимальный размер продаж: " + salesManager.max());
+        System.out.println("Среднее обрезанное количество продаж: " +salesManager.medium());
     }
 }
